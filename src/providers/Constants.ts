a3542d3277150c67d8e14767ee4622cb58d007f0
AMI-zz <810701542@qq.com>
2018-01-12 14:46:19 +0800

    first commit

---
+++ src/providers/Constants.ts
@@ -0,0 +1,9 @@
+
+
+// URL地址
+
+export const GOODLIST_URL = 'http://rapapi.org/mockjsdata/18396/api/discove/goodlist';
+export const GOODLIST_head_URL = 'http://img.lapin365.com/productpictures';
+
+
+
